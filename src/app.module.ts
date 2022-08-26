6cb6e50e7b539611c29db73a2bb6e8b7b9049ece
Verry Gusti Andrea <verrygustiandrea@gmail.com>
2022-08-26 15:34:39 +0700

    env push

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({
+    envFilePath: ['.env.development.local'],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
