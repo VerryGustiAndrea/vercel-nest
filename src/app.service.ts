2045004a3136efb5ef68ad506199b52f3da5396e
Verry Gusti Andrea <verrygustiandrea@gmail.com>
2022-08-26 16:35:13 +0700

    env push

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'testingS'+ process.env.TEKS;
+    return 'testing'+ process.env.TEKS;
     
   }
 }
